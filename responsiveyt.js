d5febd35751d4611e2d191de2f7e5af022212dc2
FulfilMyRequest <onlyrequested@gmail.com>
2016-04-14 22:27:16 +0530

    Update responsiveyt.js

--- responsiveyt.js
+++ responsiveyt.js
@@ -12,7 +12,7 @@ function labnolThumb(id) {
 };
 function labnolIframe() {
     var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "//www.youtube-nocookie.com/embed/" + this.parentNode.dataset.id + "?enablejsapi=1&showinfo=0&iv_load_policy=3&html5=1&rel=0&autoplay=1");
+    iframe.setAttribute("src", "//www.youtube-nocookie.com/embed/" + this.parentNode.dataset.id + "?enablejsapi=1&showinfo=0&iv_load_policy=3&html5=1&rel=0&autoplay=1&autohide=1");
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("id", "youtube-iframe");
     this.parentNode.replaceChild(iframe, this);
@@ -32,7 +32,7 @@ function dmnThumb(id) {
 };
 function dmnIframe() {
     var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "//www.dailymotion.com/embed/video/" + this.parentNode.dataset.id + "?enablejsapi=1&showinfo=0&iv_load_policy=3&rel=0&autoplay=1");
+    iframe.setAttribute("src", "//www.dailymotion.com/embed/video/" + this.parentNode.dataset.id + "?enablejsapi=1&showinfo=0&iv_load_policy=3&rel=0&autoplay=1&autohide=1");
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("id", "dmn-iframe");
     this.parentNode.replaceChild(iframe, this);
